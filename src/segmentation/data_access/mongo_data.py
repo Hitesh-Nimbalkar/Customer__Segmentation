f4db9c923e4946c1033ff396660c8f8d7c5a8dc3
Hitesh <nimbalkarhitesh@gmail.com>
2024-01-12 13:10:47 +0530

    mongo update

---
+++ src/segmentation/data_access/mongo_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import logging
+from segmentation.exception.exception import ApplicationException
+from segmentation.data_access.mongo_access import mongo_client
+import sys
+
+def get_collection_as_dataframe(database_name: str, collection_name: str) -> pd.DataFrame:
+    if not database_name or not collection_name:
+        raise ValueError("Database name and collection name cannot be empty.")
+
+    try:
+        with mongo_client() as client:
+            logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
+            df = pd.DataFrame(list(client[database_name][collection_name].find()))
+            # Drop unnamed column if it exists
+            if 'Unnamed: 0' in df.columns:
+                df.drop(columns=['Unnamed: 0'], inplace=True)
+
+            if "_id" in df.columns:
+                logging.info(f"Dropping column: _id ")
+                df = df.drop("_id", axis=1)
+
+            logging.info(f"Rows and columns in df: {df.shape}")
+            return df
+
+    except ValueError as ve:
+        raise ApplicationException(ve, sys.exc_info())
+    except Exception as e:
+        raise ApplicationException(e, sys.exc_info())
